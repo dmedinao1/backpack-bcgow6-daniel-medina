f4023093266c4ab6c2cc5ff09a2fadd797f88afd
dmedinao1 <daniel.medina@mercadolibre.com.co>
2022-09-23 10:54:55 -0500

    23-09-C1-GO-Bases-TM

---
+++ 23-09-C1-GO-Bases-TM/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Ejercicio 1 - Imprimí tu nombre
+	var nombre = "Daniel Medina"
+	direccion := "Calle 75a sur, Bogotá"
+
+	fmt.Println("Nombre: ", nombre, "Dirección: ", direccion)
+
+	// Ejercicio 2 - Clima
+	var temperatura float32 = 8.5
+	var humedad string = "77%"
+	var presion int = 1029
+
+	fmt.Println("Temperatura:", es_valido)
+	fmt.Println("Humedad:", humedad)
+	fmt.Println("Presión:", presion)
+
+	// Ejercicio 4
+	var apellido string = "Gomez"
+	var edad int = "35"
+	boolean := false
+	var sueldo string = 45857.90
+	var nombre string = "Julián"
+
+	fmt.Println("Presión:", boolean)
+
+}
+
+const es_valido = true
